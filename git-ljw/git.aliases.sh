4ced97f9d632cf27d1ba4d06d350e4cbea75cb22
lijunwei <lijunwei@udesk.cn>
2021-09-18 01:00:53 +0800

    Commit manually.

---
+++ git-ljw/git.aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+alias 2='git checkout - && echo && git log | head -n1'
+alias ccc='git add .; git commit -m "CODE CLEANUP."; git push'
+alias cm='gaa;gcmsg "$(prepend_commit_message)Commit manually.";gp;'
+alias c='cm'
+alias d='git diff --staged | tig'
+alias fix="gaa;gcmsg 'FIX.';gp;"
+alias g='git show HEAD | tig'
+alias gbb='git branch -vv | cat'
+alias gcam='git commit -v -a --no-edit --amend'
+alias gcob="git checkout -b"
+alias gpo='git push origin '
+alias groh='git reset --hard HEAD@{u}'
+alias gst='git status && git log | head -n1'
+alias jj="gaa;gcmsg 'DEBUG.';gp;"
+alias t="tig"
+alias ts='tig show HEAD'
+alias tt='tig --all'
