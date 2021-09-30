21bd9cb732323e27fbff5bb12e07efdc8b31db32
lijunwei <lijunwei@udesk.cn>
2021-09-30 18:29:57 +0800

    Commit manually.

---
+++ git-ljw/git.local.repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO
+#
+# 1. make dir in $HOME/local.repo
+# 2. unset current repo's origin
+# 3. set local_repo as current repo's origin(`git remote add origin file://$HOME/local.repo/demo.git`)
+# 4. push
+function git-push-local-repo() {
+  echo "Hi from git-push-local-repo..."
+}
