db896de63a70ac84e92472efa5e09f39c41ae413
lijunwei <lijunwei@udesk.cn>
2021-09-30 18:33:22 +0800

    Commit manually.

--- git-ljw/git.local.repo.sh
+++ git-ljw/git.local.repo.sh
@@ -7,5 +7,14 @@
 # 3. set local_repo as current repo's origin(`git remote add origin file://$HOME/local.repo/demo.git`)
 # 4. push
 function git-push-local-repo() {
+  echo "current remote config:"
+  echo "------------------------"
+  git remote -vv
+  echo
+  echo "Rollback method:"
+  echo "------------------------"
+  echo "This is how I Rollback..."
+
+  echo
   echo "Hi from git-push-local-repo..."
 }
