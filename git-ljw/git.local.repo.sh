e6693606498c0303595a87cefe920407063f0982
lijunwei <lijunwei@udesk.cn>
2021-10-01 16:34:34 +0800

    Commit manually.

--- git-ljw/git.local.repo.sh
+++ git-ljw/git.local.repo.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-function trim_remote_info(){
+# TODO 不要泄露这么多无用的function
+
+function do_trim_remote_info(){
+  local info="$1"
+  echo $info | sed 's/origin//' | sed 's/fetch//'  | sed 's/push//' | sed 's/()//'
+}
+
+function do_setup_local_repo(){
+  local local_repo_dir="$HOME/local.repo"
+  cd $local_repo_dir
+
+  local local_repo_name="$1"
+  git init --bare "${local_repo_name}.git"
+  echo "$local_repo_dir/${local_repo_name}.git"
+}
+
+function do_print_rollback_info(){
+  local info="$1"
+  echo $info | sed 's/origin//' | sed 's/fetch//'  | sed 's/push//' | sed 's/()//'
+}
+
+function do_rollback(){
   local info="$1"
   echo $info | sed 's/origin//' | sed 's/fetch//'  | sed 's/push//' | sed 's/()//'
 }
@@ -21,7 +42,7 @@ function git-push-local-repo() {
   echo
   echo "Rollback method:"
   echo "------------------------"
-  local rollback_origin_push=$(trim_remote_info "$(tail -n1 $backup_file)")
+  local rollback_origin_push=$(do_trim_remote_info "$(tail -n1 $backup_file)")
   cat << EOF
 git remote remove origin
 git remote add origin $rollback_origin_push
