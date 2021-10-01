17a52af9a90ae23ac94f264b3c2cb521027d2cce
lijunwei <lijunwei@udesk.cn>
2021-10-01 16:57:27 +0800

    Commit manually.

--- git-ljw/git.local.repo.sh
+++ git-ljw/git.local.repo.sh
@@ -7,8 +7,19 @@ function do_trim_remote_info(){
   echo $info | sed 's/origin//' | sed 's/fetch//'  | sed 's/push//' | sed 's/()//'
 }
 
+function git_remote_backup_file(){
+  echo "${_git_ljw_path}/git.local.repo.remote.backup"
+}
+
 function do_log_rollback_info(){
-  local file="${_git_ljw_path}/git.local.repo.remote.backup"
+  git remote -vv >> /dev/null
+
+  if [[ "$?" != 0 ]]; then
+    echo "No need to backup"
+    return 1
+  fi
+
+  local file=$(git_remote_backup_file)
   echo "Backup current remote config"
   echo "------------------------"
   git remote -vv
@@ -40,7 +51,10 @@ function do_setup_local_repo(){
   cd $local_repo_dir
 
   local local_repo_name="$1"
-  git init --bare "${local_repo_name}.git"
+  git init --bare "${local_repo_name}.git" >> $(git_remote_backup_file)
+
+  echo "Local Repo"
+  echo "------------------------"
   echo "$local_repo_dir/${local_repo_name}.git"
 }
 
@@ -56,8 +70,13 @@ function do_rollback(){
 # 3. set local_repo as current repo's origin(`git remote add origin file://$HOME/local.repo/demo.git`)
 # 4. push
 function git-push-local-repo() {
+  local local_repo_name=$1
+  if [[ "" == "$local_repo_name" ]]; then
+    local local_repo_name="tmp_$(date "+%Y%m%d%H%M%S")"
+  fi
+
   do_log_rollback_info
   echo
-  # do_setup_local_repo
+  do_setup_local_repo "$local_repo_name"
   echo "Hi from git-push-local-repo..."
 }
