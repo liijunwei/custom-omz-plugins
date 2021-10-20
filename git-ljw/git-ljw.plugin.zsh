1bc370109b1ff694a538cb81bccbe64aa3f9af2c
lijunwei <lijunwei@udesk.cn>
2021-10-20 09:12:56 +0800

    Add note on plugin entry

--- git-ljw/git-ljw.plugin.zsh
+++ git-ljw/git-ljw.plugin.zsh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Plugin entry
-_git_ljw_path=$(dirname $(realpath $BASH_SOURCE))
+# _git_ljw_path=$(dirname $(realpath $BASH_SOURCE)) # on linux
+_git_ljw_path="$(dirname -- $0)" # on macos
 
 for f in $(ls $_git_ljw_path/*.sh); do
   source $f;
