9aeadfb773509251f52ecaab47fe6682d3d3940f
lijunwei <ljw532344863@sina.com>
2022-02-03 15:17:11 +0800

    Modify git-grep-commits
    + using fzf(the command-line fuzzy finder)

--- git-ljw/git.grep.in.all.commits.sh
+++ git-ljw/git.grep.in.all.commits.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # grep keyword in all commits from current project
+# prerequisite: https://github.com/junegunn/fzf
 function git-grep-commits() {
   local keyword=$1
-  git log -S "$keyword" --source --all | cat
+  git log -S "$keyword" --source --all | fzf
 }
