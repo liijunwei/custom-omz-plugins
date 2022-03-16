46e1a9e52a1f2672abcdd3903a7e2f0fb0c9b9d3
lijunwei <ljw532344863@sina.com>
2022-03-16 18:26:55 +0800

    Code cleanup.

--- git-ljw/git.grep.in.all.commits.sh
+++ git-ljw/git.grep.in.all.commits.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # grep keyword in all commits from current project
-# prerequisite: https://github.com/junegunn/fzf
 function git-grep-commits() {
   local keyword=$1
   git log -S "$keyword" --source --all | cat
