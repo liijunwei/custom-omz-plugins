3e6f05aedd1f42dbac63e9dfb440efb76c54efa0
lijunwei <ljw532344863@sina.com>
2022-04-13 23:05:44 +0800

    Migrate to omz-git/omz-thought/omz-misc.

--- git-ljw/git.grep.in.all.commits.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# grep keyword in all commits from current project
-function git-grep-commits() {
-  local keyword=$1
-  git log -S "$keyword" --source --all | cat
-}
--- thought/README.md
+++
@@ -1,7 +0,0 @@
-# Add thought to blog
-
-```bash
-thought "hello world"
-```
-
-就一个用处: 使用命令行随时记录所思所想...
